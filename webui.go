650c1a990ba060674c8aeb41ff8592ed800022b8
VanessaGregoriou <vanessa.e.gregoriou@gmail.com>
2016-01-08 10:23:31 +1100

    merge fixes

--- webui.go
+++ webui.go
@@ -143,19 +143,19 @@ func init() {
 	}(fromDate, toDate, db)
 
 	log.Println("Initial cache download")
-	// refreshLocalCache()
+	refreshLocalCache()
 
-	// // goroutine to update local cache if there has been any change to database
-	// count := 1
-	// go func(count int) {
-	// 	for {
-	// 		if checkDBUpdateTime("questions", mostRecentUpdate) {
-	// 			log.Printf("Refreshing cache %v", count)
-	// 			refreshLocalCache()
-	// 			count++
-	// 		}
-	// 	}
-	// }(count)
+	// goroutine to update local cache if there has been any change to database
+	count := 1
+	go func(count int) {
+		for {
+			if checkDBUpdateTime("questions", mostRecentUpdate) {
+				log.Printf("Refreshing cache %v", count)
+				refreshLocalCache()
+				count++
+			}
+		}
+	}(count)
 
 	http.HandleFunc("/login", authHandler)
 	http.HandleFunc("/", handler)
